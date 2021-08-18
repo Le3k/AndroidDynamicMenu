bf5cbe9ff937b29f5957efc0cbe178f15e06c0fa
Andrei Cojocaru <andrei.cojocaru@hansenhof.de>
2021-08-18 10:54:40 +0300

    Initial commit

---
+++ app/src/main/java/com/example/menutest/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.menutest;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuBuilder;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private static final int GROUP_ID = 1000;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        int firstItemOrder = menu.findItem(R.id.menu_item_1).getOrder();
+        MenuItem groupTitleItem = menu.add(GROUP_ID, Menu.NONE, firstItemOrder + 5, "Dynamic group");
+        MenuItem groupItem1 = menu.add(GROUP_ID, Menu.NONE, firstItemOrder + 5, "Dynamic item 1");
+        MenuItem groupItem2 = menu.add(GROUP_ID, Menu.NONE, firstItemOrder + 5, "Dynamic item 2");
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.menu_item_1) {
+            // clicked menu item 1
+            return true;
+        }
+        if (id == R.id.menu_item_2) {
+            // clicked menu item 2
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
